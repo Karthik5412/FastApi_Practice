f99a001fd6ac71e385a773dcaca773da61f5a4ae
Karthik Kurucharlapati <155161592+Karthik5412@users.noreply.github.com>
2026-04-01 10:50:03 +0530

    only get completed

--- Advanced/main.py
+++ Advanced/main.py
@@ -21,14 +21,27 @@ def item_by_id (id : int) :
 
 @app.get('/product')
 def search_by_name(name : str = Query(default= None, max_length= 35, min_length= 3, 
-                                    description= 'Search item by name')) -> list[dict]:
+                                    description= 'Search item by name'
+                                    ),
+                    sort_status : bool = Query(default = False, description= 'Sorting'),
+                    order : str = Query(default= 'asc', description= 'Specify order')
+                    ):
     target = name.lower()
     
     if name :
-        items = [i for i in products if target in i['name'].lower() ] 
+        items = [i for i in products if target in i['name'].lower() ]  
         
-        return items 
     
-    else :
-        return [{}]
+    if not items :
+        return 'No Such Items'
+    
+    if sort_status :
+        
+        reverse = order == 'desc'
+        
+        
+        result = sorted(items, key= lambda x : x.get('price',0), reverse=reverse)
+        
+        
+        return result 
     
