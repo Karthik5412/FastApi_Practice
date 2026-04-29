27e30993a5d7548801ff015ceb37152272863932
Karthik Kurucharlapati <155161592+Karthik5412@users.noreply.github.com>
2026-04-29 13:02:47 +0530

    Added update classes

--- New_Start/product_schema.py
+++ New_Start/product_schema.py
@@ -2,6 +2,8 @@ from pydantic import BaseModel, Field, AnyUrl, EmailStr, field_validator, model_
 from uuid import UUID
 from typing import Annotated, Optional, List
 
+# CREATE MODEL
+
 # "dimensions": 
 class dimension_of_product(BaseModel) :
     weight : Annotated[float, Field(le= 25, ge= 5, default= 5)]
@@ -14,7 +16,7 @@ class seller_model(BaseModel):
     seller_rating : Annotated[float, Field(lt= 5, gt= 0, default= None)]
     is_verified : Annotated[bool, Field(default= None, strict= True)]
 
-
+# Core Model
 class product(BaseModel):
     
     # "id"
@@ -142,5 +144,101 @@ class product(BaseModel):
     @property
     def final_price(self) -> float :
         return round(self.price * (1 - self.discount/100), 2)
+
+# UPDATE MODEL
+
+# dimensions
+class dimension_of_product_update(BaseModel) :
+    weight : Optional[float]= Field(le= 25, ge= 5, default= 5)
+    height : Optional[float]= Field(le= 2, ge= 0.2, default= 0.5)
+    depth : Optional[float]= Field(le= 50, ge= 5, default= 10)
+
+# Seller Model
+class seller_model_update(BaseModel):
+    seller_name : Optional[str]= Field(min_length=7,max_length= 25, default='John Doe')
+    seller_rating : Optional[float]=  Field(lt= 5, gt= 0, default= None)
+    is_verified : Optional[bool]=  Field(default= None, strict= True)
+
+# Core Model
+class product_update(BaseModel):
+
+    # "product_name"
+    product_name : Annotated[
+        str,
+        Field(
+            title= 'Product name',
+            strict= True,
+            max_length= 25,
+            min_length= 5,
+            description= 'Write the name of product',
+            examples=['Silent Click Mouse','Noise Cancelling Headphones']
+    )]
+    
+    # "price"
+    price : Optional[float] = Field(ge=30, title='Price', examples=['1249.99'])
+    
+    
+    # "is_in_stock"
+    is_in_stock : Optional [bool] = Field(title= 'In stock', strict= True)
+    
+    # "dimensions"
+    dimensions : Optional[dimension_of_product_update]
+    
+    # "tags"
+    tags : Optional[List[str]] = Field(title= 'Tags', default= None, max_length= 10, description='Category of product')
+    
+    # "manufacturer_email"
+    manufacturer_email : Optional[EmailStr] = Field(examples=['johdoe@gmail.com'])
+    
+    # "rating": 
+    rating : Optional[float] = Field(default=2.25, lt=5, gt=0, description='Rate about the product')
+    
+    # "warehouse_location"
+    warehouse_location : Optional[str] = Field(max_length= 5, min_length=3, examples=["WA-01"])
+    
+    
+    # "shipping_code"
+    shipping_code : Optional[str]=Field(max_length= 50, min_length=15, examples=["SHP-WA-01-c552d4e8-128f-4609-ae06-42310fd7b35c"])
+    
+    
+    #"quantity_available"
+    quantity_available : Optional[int]=Field(ge=0, description='Quantity of product')
+    
+    
+    #"discount"
+    discount : Optional[float]= Field(default=5, le=50, ge=0, description='Available Discount')
+    
+    
+    
+    # "seller_data"
+    seller_data : Optional[seller_model_update]
+    
+    
+    @field_validator('warehouse_location', mode='after')
+    @classmethod
+    def warehouse_validation(cls, val: str):
+        if '-' not in val:
+            raise ValueError('Need to put "-"')
+        
+        if not (val.split('-')[0].isupper() and  val.split('-')[-1].isdigit()) :
+            raise ValueError('Ending three must end up like this "KD-12"')
+        
+        return val
+    
+    @model_validator(mode='after')
+    @classmethod
+    def validating_shipping(cls, model : product) :
+        
+        if model.is_in_stock == True and model.quantity_available == 0 :
+            raise ValueError('If Stock how quaintity is 0')
+        
+        if not model.warehouse_location in model.shipping_code :
+            raise ValueError('This Address is not correct')
+        
+        return model
+
+
+
+
 
 
